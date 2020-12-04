9f88968b13d53b448f93a99732483c27ca9fc900
Fukuoka-desu <fuyukun39@gmail.com>
2020-12-04 18:07:32 +0900

    create button

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
-<!doctype html>
-<html class="no-js" lang="">
+const question = 'ゲーム市場、最も売れたゲーム機は？';
+const answers = ['スーパーファミコン',
+                 'プレイステーション２',
+                 'ニンテンドースイッチ',
+                 'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
 
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
+document.getElementById('js-question').textContent = question;
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-  <!-- Place favicon.ico in the root directory -->
 
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
+const $button = document.getElementsByTagName('button');
 
-  <meta name="theme-color" content="#fafafa">
-</head>
+let buttonIndex = 0;
+let buttonLength = $button.length;
+while(buttonIndex < $button.length){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+}
 
-<body>
-
-  <div class="container">
-    <div class="mt-3 alert alert-primary" role="alert">
-      A simple primary alert—check it out!
-    </div>
-    <div class="d-flex justify-content-center">
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="ml-1 btn btn-danger">Danger</button>
-      <button type="button" class="ml-1 btn btn-danger">Danger</button>
-      <button type="button" class="ml-1 btn btn-danger">Danger</button>
-     </div>
-  </div>
-
-  <script src="app.js"></script>
-</body>
-
-</html>
+//ボタンをクリックしたら正誤判定
+$button[0].addEventListener('click',() => {
+if (correct === $button[0].textContent) {
+   window.alert('正解!') ;
+} else {
+   window.alert('不正解!');    
+}
+});
+$button[1].addEventListener('click',() => {
+    if (correct === $button[1].textContent) {
+       window.alert('正解!') ;
+    } else {
+       window.alert('不正解!');    
+ }
+ });
+ $button[2].addEventListener('click',() => {
+    if (correct === $button[2].textContent) {
+       window.alert('正解!') ;
+    } else {
+       window.alert('不正解!');    
+    }
+});
+$button[3].addEventListener('click',() => {
+    if (correct === $button[3].textContent) {
+       window.alert('正解!') ;
+    } else {
+       window.alert('不正解!');    
+    }
+});
